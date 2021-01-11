920ac731600a7c8e082c3ac5debcfe75d5d1d15b
abdalem <abdennour@abbi-studio.fr>
2021-01-11 03:44:59 +0100

    rebuild package

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
-import { CompatibleCases } from './types';
+import { CompatibleCasesConstants, CompatibleCases, Cases } from './types';
 declare const caseize: (data: any, casing: CompatibleCases) => any;
 export default caseize;
+export { CompatibleCasesConstants, CompatibleCases, Cases };
--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CompatibleCasesConstants = void 0;
 const lodash_1 = __importDefault(require("lodash"));
 const types_1 = require("./types");
+Object.defineProperty(exports, "CompatibleCasesConstants", { enumerable: true, get: function () { return types_1.CompatibleCasesConstants; } });
 const cases = {
     [types_1.CompatibleCasesConstants.CAMELCASE]: (key) => lodash_1.default.camelCase(key),
     [types_1.CompatibleCasesConstants.SNAKECASE]: (key) => lodash_1.default.snakeCase(key),
