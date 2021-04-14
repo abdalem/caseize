82ae66f98e9e455158673dcaa89637a75b83c744
abdalem <abdennour@abbi-studio.fr>
2021-04-14 11:11:51 +0200

    fix reference issue

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
-import _ from 'lodash';
-import { CompatibleCasesConstants, CompatibleCases, Cases, Caseize } from './types';
+import _ from "lodash";
+import {
+  CompatibleCasesConstants,
+  CompatibleCases,
+  Cases,
+  Caseize,
+} from "./types";
 
 const cases: Cases = {
-  [ CompatibleCasesConstants.CAMELCASE ]: (key: string) => _.camelCase(key),
-  [ CompatibleCasesConstants.SNAKECASE ]: (key: string) => _.snakeCase(key),
+  [CompatibleCasesConstants.CAMELCASE]: (key: string) => _.camelCase(key),
+  [CompatibleCasesConstants.SNAKECASE]: (key: string) => _.snakeCase(key),
 };
 
 const caseize: Caseize = (data, casing) => {
-  if(_.isArray(data)) data = _.map(data, (item: any) => caseize(item, casing));
-  
-  if(_.isPlainObject(data)) {
-    _.each(_.keys(data), (key: string) => {
-      const value = data[key];
-      delete data[key];
-      data[cases[casing](key)] = caseize(value, casing);
+  let dataCopy = JSON.parse(JSON.stringify(data));
+  if (_.isArray(dataCopy))
+    dataCopy = _.map(dataCopy, (item: any) => caseize(item, casing));
+
+  if (_.isPlainObject(dataCopy)) {
+    _.each(_.keys(dataCopy), (key: string) => {
+      const value = dataCopy[key];
+      delete dataCopy[key];
+      dataCopy[cases[casing](key)] = caseize(value, casing);
     });
   }
 
-  return data;
-}
+  return dataCopy;
+};
 
 export default caseize;
 export { CompatibleCasesConstants, CompatibleCases, Cases };
